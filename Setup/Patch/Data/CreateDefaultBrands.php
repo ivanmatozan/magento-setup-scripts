08aab61b7a4d3b1adee583edac903d1d3e5c87db
Ivan Matozan <ivan.matozan@gmail.com>
2019-08-06 20:25:42 +0200

    Declarative Schema data patch

---
+++ Setup/Patch/Data/CreateDefaultBrands.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Matozan\Magento\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchInterface;
+
+class CreateDefaultBrands implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    public function __construct(ModuleDataSetupInterface $moduleDataSetup)
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply()
+    {
+        $brands = [
+            ['name' => 'Sike', 'description' => 'Something cool'],
+            ['name' => 'Luma', 'description' => 'Something not quite as cool'],
+            ['name' => 'Babidas', 'description' => 'To cool to care']
+        ];
+
+        $records = array_map(static function ($brand) {
+            return array_merge($brand, ['is_enabled' => 1, 'website_id' => 1]);
+        }, $brands);
+
+        $this->moduleDataSetup->getConnection()->insertMultiple(
+            'matozan_brand_example',
+            $records
+        );
+
+        return $this;
+    }
+}
