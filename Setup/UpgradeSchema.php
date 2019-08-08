f0432c6e9fc519ab1fa78604205fa0180c7be3ee
Ivan Matozan <ivan.matozan@gmail.com>
2019-08-08 18:55:06 +0200

    Inspecting a table structure

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -15,6 +15,23 @@ class UpgradeSchema implements UpgradeSchemaInterface
      */
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        echo __METHOD__ . PHP_EOL;
+        $tableName = 'mage2tv_example_brand';
+
+        // Check if table exists
+        $setup->tableExists($tableName);
+        $setup->getConnection()->isTableExists($setup->getTable($tableName));
+
+        // Check if column exists
+        $setup->getConnection()->tableColumnExists(
+            $setup->getTable($tableName),
+            'description'
+        );
+
+        // Get detailed information about columns
+        $setup->getConnection()->describeTable($setup->getTable($tableName));
+
+        // Listing indexes and foreign keys
+        $setup->getConnection()->getIndexList($setup->getTable($tableName));
+        $setup->getConnection()->getForeignKeys($setup->getTable($tableName));
     }
 }
