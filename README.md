bfddab109b5cb6b2f8dcc8dad3a5e3c95c82406c
Ivan Matozan <ivan.matozan@gmail.com>
2019-08-10 10:48:13 +0200

    Add custom product attribute

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ A Magento 2 module for learning.
 - php bin/magento setup:upgrade --dry-run=1 (var/log/dry-run-installation.log)
 - php bin/magento setup:upgrade --safe-mode=1 (var/declarative_dumps_csv/FILE.csv)
 - php bin/magento setup:upgrade --data-restore=1 (--safe-mode=1)
+
+---
+
+### EAV property mappers
+- Magento\Eav\Model\Entity\Setup\PropertyMapperInterface
+- Magento\Eav\Model\Entity\Setup\PropertyMapper\Composite
+- Magento\Eav\Model\Entity\Setup\PropertyMapper
+- Magento\Catalog\Model\ResourceModel\Setup\PropertyMapper
+- Magento\Customer\Model\ResourceModel\Setup\PropertyMapper
+- Magento\ConfigurableProduct\Model\ResourceModel\Setup\PropertyMapper
+- Magento\CatalogSearch\Model\ResourceModel\Setup\PropertyMapper
+
+---
--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Matozan\Magento\Setup;
 
+use Magento\Catalog\Api\Data\ProductAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
@@ -11,10 +13,48 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 class InstallData implements InstallDataInterface
 {
     /**
+     * @var EavSetup
+     */
+    private $eavSetup;
+
+    public function __construct(EavSetup $eavSetup)
+    {
+        $this->eavSetup = $eavSetup;
+    }
+
+    /**
      * @inheritDoc
      */
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        echo __METHOD__ . PHP_EOL;
+        $this->createProductAttribute();
+    }
+
+    private function createProductAttribute(): void
+    {
+        $attributeCode = 'legacy_sku';
+        $entityType = ProductAttributeInterface::ENTITY_TYPE_CODE;
+
+        $setId = $this->eavSetup->getDefaultAttributeSetId($entityType);
+        $groupId = $this->eavSetup->getDefaultAttributeGroupId($entityType, $setId);
+        $groupName = $this->eavSetup->getAttributeGroup(
+            $entityType,
+            $setId,
+            $groupId,
+            'attribute_group_name'
+        );
+
+        $this->eavSetup->addAttribute($entityType, $attributeCode, [
+            'label' => 'Legacy SKU',
+            'required' => 0,
+            'user_defined' => 1,
+            'unique' => 1,
+            'searchable' => 1,
+            'visible_on_front' => 1,
+            'visible_in_advanced_search' => 1,
+            'is_used_in_grid' => 1,
+            'group' => $groupName,
+            'sort_order' => 30
+        ]);
     }
 }
