1f9d473ee15f2a130ad89f163f1c1b2ff6519c1e
Ivan Matozan <ivan.matozan@gmail.com>
2019-09-03 19:57:23 +0200

    Create Index controller

---
+++ Controller/Index/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Matozan\Magento\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
+
+class Index extends Action
+{
+    /**
+     * @inheritDoce
+     */
+    public function execute()
+    {
+        /** @var \Magento\Framework\Controller\Result\Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents(__CLASS__);
+
+        return $result;
+    }
+}
